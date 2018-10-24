4a8d9c15d4eb7b50add8b533805ce9b42fb66cbb
Lars <larsmackenzie@gmail.com>
2018-10-24 16:05:27 -0500

    created heroku build

---
+++ build/precache-manifest.f6adb6728a70bd35efaffc36f88d04bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18e5c953bbce4b6a81bc",
+    "url": "/static/js/main.18e5c953.chunk.js"
+  },
+  {
+    "revision": "84cb39d36f431610d4c8",
+    "url": "/static/js/1.84cb39d3.chunk.js"
+  },
+  {
+    "revision": "18e5c953bbce4b6a81bc",
+    "url": "/static/css/main.dace7ed9.chunk.css"
+  },
+  {
+    "revision": "2d891c3a3e0f5ac4c4e9191ca0eeec20",
+    "url": "/index.html"
+  }
+];
